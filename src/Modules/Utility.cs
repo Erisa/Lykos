64c1d9227c0f13513a903f600a3fdb12c4711c1e
Erisa Arrowsmith <seriel@fl0.co>
2018-12-12 05:58:36 +0000

    Put the ping command back after accidentally deleting it

--- src/Modules/Utility.cs
+++ src/Modules/Utility.cs
@@ -71,4 +71,15 @@ namespace Lykos.Modules
             await ctx.RespondAsync($"My prefixes are: ```json\n{JsonConvert.SerializeObject(Program.cfgjson.Prefixes)}```");
         }
     }
+
+    public async Task Ping(CommandContext ctx)
+    {
+        DSharpPlus.Entities.DiscordMessage return_message = await ctx.Message.RespondAsync("Pinging...");
+        ulong ping = (return_message.Id - ctx.Message.Id) >> 22;
+        Char[] choices = new Char[] { 'a', 'e', 'o', 'u', 'i', 'y' };
+        Char letter = choices[Program.rnd.Next(0, choices.Length)];
+        await return_message.ModifyAsync($"P{letter}ng! 🏓\n" +
+            $"• It took me `{ping}ms` to reply to your message!\n" +
+            $"• Last Websocket Heartbeat took `{ctx.Client.Ping}ms`!");
+    }
 }
