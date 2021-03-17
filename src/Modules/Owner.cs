7e84da402d06ed31c0a3879e9ce601897f228ee5
Erisa A <seriel@erisa.moe>
2021-03-17 23:52:51 +0000

    Fix build errors

--- src/Modules/Owner.cs
+++ src/Modules/Owner.cs
@@ -2,12 +2,14 @@
 using DSharpPlus.CommandsNext.Attributes;
 using DSharpPlus.Entities;
 using Minio.Exceptions;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Net;
 using System.Net.Http;
 using System.Runtime.InteropServices;
+using System.Text;
 using System.Threading.Tasks;
 using static Lykos.Modules.Helpers;
 
