484031cc79b2a0f443e6a073ddf66869158fa051
Erisa A <erisa@erisa.uk>
2025-03-24 00:42:56 +0000

    set max tokens

--- src/Program.cs
+++ src/Program.cs
@@ -384,6 +384,7 @@ namespace Lykos
                 conversations[channel.Id] = openai.Chat.CreateConversation(new ChatRequest()
                 {
                     Model = cfgjson.OpenAI.Model,
+                    MaxTokens = 4000
                 });
                 response = "`[a potential history issue was detected so the history was reset! in future this will be handled in a cleaner way]`\n";
 
